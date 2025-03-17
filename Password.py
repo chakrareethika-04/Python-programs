dbc5712f2ad1722181dc206d671bae04c046525f
chakrareethika-04 <reethikadasari2004@gmail.com>
2025-03-17 16:45:27 +0530

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,19 @@
+password=input()
+l=len(password)
+if l<8:
+    print("invalid password")
+    digit=False
+    upper=False
+    lower=False
+for i in password:
+  if i.isdigit():
+2
+    digit=True
+  elif i.isupper():
+    upper=True
+  elif i.islower():
+    lower=True
+if digit and upper and lower:
+  print("valid password")
+else:
+   print("Invalid password")
