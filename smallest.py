0f08fd07be21e2c4738bc7c792b2f6b868f39f60
chakrareethika-04 <reethikadasari2004@gmail.com>
2025-03-17 16:46:47 +0530

    Create smallest.py

---
+++ smallest.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a==b==c:
+  print("a,b,c is smallest")
+elif a<=b and a<=c:
+  print("a is smallest")
+elif b<=a and b<=c :
+  print("b is smallest")
+else:
+   print("c is smallest")
