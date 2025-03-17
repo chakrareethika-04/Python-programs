45412e2b5b3dac5b0d87f26655610fb255c302e5
chakrareethika-04 <reethikadasari2004@gmail.com>
2025-03-17 15:06:30 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,35 @@
+
+a=int(input())
+operator=input()
+b=int(input())
+if operator=='+':
+  print(a+b)
+elif operator=='-':
+  print(a-b)
+elif operator=='*':
+  print(a*b)
+elif operator=='/':
+  print(a/b)
+elif operator=='**':
+  print(a**b)
+elif operator=='//':
+  print(a//b)
+elif operator=='%':
+  print(a%b)1. calculator
+a=int(input())
+operator=input()
+b=int(input())
+if operator=='+':
+  print(a+b)
+elif operator=='-':
+  print(a-b)
+elif operator=='*':
+  print(a*b)
+elif operator=='/':
+  print(a/b)
+elif operator=='**':
+  print(a**b)
+elif operator=='//':
+  print(a//b)
+elif operator=='%':
+  print(a%b)
